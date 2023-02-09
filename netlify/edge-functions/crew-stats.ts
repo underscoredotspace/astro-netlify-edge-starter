9e14d09cd86b910c0a1808e9bfb0764a5182374d
Colin Tindle <underscoredotspace@users.noreply.github.com>
2023-02-09 19:07:27 +0000

    js is hard

--- netlify/edge-functions/crew-stats.ts
+++ netlify/edge-functions/crew-stats.ts
@@ -2,7 +2,7 @@ export default async (request: Request) => {
     const crewStats = await fetch('https://gpadcrew.com/api/member/count')
         .then((res) => res.json());
 
-    return new Response(JSON.Stringify(crewStats), {
+    return new Response(JSON.stringify(crewStats), {
         status: 200,
         headers: {
         "Content-Type": "application/json"
